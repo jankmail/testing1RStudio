41c5d2f0bb4c866db7990bf29e1c7482017614ae
jankmail <85782231+jankmail@users.noreply.github.com>
2021-06-13 18:40:37 +0200

    Add files via upload

---
+++ testing1RStudioErsterAbgleichMItGit.R
@@ -0,0 +1,12 @@
+
+print("This file was created with RStudio")
+
+print("ANd now it lives on GitHUb")
+
+print("New test of commit")
+
+print("Sth goes wrong")
+
+print("Going to help")
+
+print ("almost do have it")
